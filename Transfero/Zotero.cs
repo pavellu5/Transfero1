1ba58ca89a3b76962682ecb5ddeddd56efd94141
pavellu5 <pavellu5@fel.cvut.cz>
2015-03-20 10:29:55 +0100

    New class Zotero
    New class Zotero

--- Transfero/Zotero.cs
+++ Transfero/Zotero.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SQLite;
 
 namespace Transfero
 {
     class Zotero
     {
+        SQLiteConnection connection;
+
+        public Zotero()
+        {
+            connection = new SQLiteConnection("Data Source=D:\Zotero\zotero.sqlite;Version=3;");
+            connection.Open();
+        }
+
+
+
+
     }
 }
